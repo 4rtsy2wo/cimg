05239ea59d9852c28c133e6e05a13a03f897fe54
4rtsy2wo <120429109+4rtsy2wo@users.noreply.github.com>
2024-02-09 14:57:12 +0100

    test run

---
+++ inject.js
@@ -0,0 +1,33 @@
+window.cae = (p, t, a) => {
+  frag = document.createDocumentFragment();
+  e = document.createElementNS("http://www.w3.org/2000/svg", t);
+  for (let k in a) e.setAttribute(k, a[k]);
+  frag.append(e);
+  p.append(frag);
+  return e;
+};
+
+window.u = (i) => {
+  return document.getElementById(i);
+};
+
+window.m = (s, e, d, mi, ma, h) => {
+  ra = h ? 15 : 255;
+  return DNA[0]
+    .match(d ? /../g : /./g)
+    .slice(s, e)
+    .map((h) => (parseInt(h, 16) / ra) * (ma - mi) + mi);
+};
+
+window._TestRect = () => {
+  let r = cae(u("svg"), "rect", {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: "red",
+  });
+  r.onclick = () => {
+    console.log("clicked");
+  };
+};
